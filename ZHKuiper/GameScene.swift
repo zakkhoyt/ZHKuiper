6987048717ff530b5e6e1dcb954813e3e0190ef2
Zakk Hoyt <vaporwarewolf@gmail.com>
2016-01-03 20:24:37 -0800

    Can now draw block to block the balls

--- ZHKuiper/GameScene.swift
+++ ZHKuiper/GameScene.swift
@@ -7,39 +7,184 @@
 //
 
 import SpriteKit
+import CoreMotion
+
+
+let kMaxBBCount = 100
 
 class GameScene: SKScene {
+    
+    var motion = CMMotionManager()
+    var blockNode: SKShapeNode? = nil
+    var point1: CGPoint = CGPointZero
+    var point2: CGPoint = CGPointZero
+    var ref = CGPathCreateMutable()
+    
     override func didMoveToView(view: SKView) {
+        
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        self.physicsWorld.speed = 5;
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        self.physicsWorld.contactDelegate = self;
+        
+        var lastDate = NSDate()
+        motion.startDeviceMotionUpdatesToQueue(NSOperationQueue()) { (motion: CMDeviceMotion?, error: NSError?) -> Void in
+            if let x = motion?.gravity.x, let y = motion?.gravity.y {
+                let now = NSDate()
+                if now.timeIntervalSinceDate(lastDate) > 0.1 {
+                    lastDate = now
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        print("gravity")
+                        self.physicsWorld.gravity = CGVectorMake(CGFloat(x), CGFloat(y))
+                    })
+                }
+            }
+        }
+        
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tap:")
+        tapGestureRecognizer.numberOfTapsRequired = 4
+        self.view?.addGestureRecognizer(tapGestureRecognizer)
+        
+//        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "pan:");
+//        self.view?.addGestureRecognizer(panGestureRecognizer);
+        
+        
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
+        myLabel.text = "Kuiper!"
+        myLabel.fontSize = 72
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        myLabel.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+        myLabel.physicsBody?.dynamic = true
+        myLabel.physicsBody?.affectedByGravity = true
+        
         
         self.addChild(myLabel)
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.5, block: { () -> Void in
+            self.addSpriteAtPoint(CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height - 30))
+            }, repeats: true)
+        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            point1 = touch.locationInNode(self)
+        }
+        touch(touches, withEvent: event)
+
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touch(touches, withEvent: event)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touch(touches, withEvent: event)
+    }
+    
+    func touch(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+        for touch in touches {
+            point2 = touch.locationInNode(self)
+            if let blockNode = blockNode {
+                
+                ref = CGPathCreateMutable()
+                CGPathMoveToPoint(ref, nil, point1.x, point1.y)
+                CGPathAddLineToPoint(ref, nil, point2.x, point2.y)
+                blockNode.path = ref
+                blockNode.physicsBody = SKPhysicsBody(edgeLoopFromPath: ref)
+            } else {
+
+                blockNode = SKShapeNode()
+                CGPathMoveToPoint(ref, nil, point1.x, point1.y)
+                CGPathAddLineToPoint(ref, nil, point2.x, point2.y)
+                blockNode!.path = ref
+                blockNode!.lineWidth = 20
+                blockNode?.lineCap = .Round
+                blockNode!.fillColor = UIColor.whiteColor()
+                blockNode!.strokeColor = UIColor.whiteColor()
+                blockNode!.physicsBody = SKPhysicsBody(edgeLoopFromPath: ref)
+                blockNode!.physicsBody!.dynamic = true
+                blockNode!.physicsBody!.affectedByGravity = false
+                self.addChild(blockNode!)
+            }
+        }
+
+    }
+    
+    func addSpriteAtPoint(point: CGPoint) {
+        let random = arc4random_uniform(11)
+        var color = ZHBBNodeColor.White
+        switch random {
+        case 0:
+            color = .Black
+        case 1:
+            color = .Blue
+        case 2:
+            color = .Brown
+        case 3:
+            color = .Cyan
+        case 4:
+            color = .Green
+        case 5:
+            color = .Magenta
+        case 6:
+            color = .Orange
+        case 7:
+            color = .Purple
+        case 8:
+            color = .Red
+        case 9:
+            color = .White
+        case 10:
+            color = .Yellow
+        default:
+            color = .White
+        }
+        
+        
+        let sprite = ZHBBNode(nodeColor: color, point: point);
+
+        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        sprite.runAction(SKAction.repeatActionForever(action))
+        self.addChild(sprite)
+    }
+    
+    func tap(sender: UITapGestureRecognizer) {
+        let fadeAction = SKAction.fadeAlphaTo(0, duration: 0.3)
+        for child in self.children {
+            child.runAction(fadeAction, completion: { () -> Void in
+                child.removeFromParent()
+            })
+        }
+//        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.4 * Double(NSEC_PER_SEC)))
+//        dispatch_after(delayTime, dispatch_get_main_queue()) {
+//            self.bbs.removeAll()
+//        }
+    }
+}
+
+
+extension GameScene: SKPhysicsContactDelegate {
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        if contact.bodyA.contactTestBitMask == contact.bodyB.contactTestBitMask {
+            print("contact")
+            let bodyA = contact.bodyA.node as! ZHBBNode
+            let bodyB = contact.bodyB.node as! ZHBBNode
+            bodyA.remove()
+            bodyB.remove()
+        }
+    }
+    
+    func didEndContact(contact: SKPhysicsContact) {
+        
     }
 }
--- ZHKuiper/ZHBBNode.swift
+++ ZHKuiper/ZHBBNode.swift
@@ -6,8 +6,138 @@
 //  Copyright © 2016 Zakk Hoyt. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
+
+enum ZHBBNodeColor: UInt {
+    case Black = 0
+    case Blue = 1
+    case Brown = 2
+    case Cyan = 3
+    case Green = 10
+    case Magenta = 4
+    case Orange = 5
+    case Purple = 6
+    case Red = 7
+    case White = 8
+    case Yellow = 9
+    
+    func nameFromColor(color: ZHBBNodeColor) -> String {
+        var name = ""
+        switch color {
+        case .Black:
+            name = "black"
+        case .Blue:
+            name = "blue"
+        case .Brown:
+            name = "brown"
+        case .Cyan:
+            name = "cyan"
+        case .Green:
+            name = "green"
+        case .Magenta:
+            name = "magenta"
+        case .Orange:
+            name = "orange"
+        case .Purple:
+            name = "purple"
+        case .Red:
+            name = "red"
+        case .White:
+            name = "white"
+        case .Yellow:
+            name = "yellow"
+        }
+        return name
+    }
+    
+    func contactBitMask(color: ZHBBNodeColor) -> UInt32 {
+        var mask: UInt32 = 0
+        switch color {
+        case .Black:
+            mask = 1 << 1
+        case .Blue:
+            mask = 1 << 2
+        case .Brown:
+            mask = 1 << 3
+        case .Cyan:
+            mask = 1 << 4
+        case .Green:
+            mask = 1 << 5
+        case .Magenta:
+            mask = 1 << 6
+        case .Orange:
+            mask = 1 << 7
+        case .Purple:
+            mask = 1 << 8
+        case .Red:
+            mask = 1 << 9
+        case .White:
+            mask = 1 << 10
+        case .Yellow:
+            mask = 1 << 11
+        }
+        return mask
+    }
+}
+
 
 class ZHBBNode: SKSpriteNode {
 
+    
+    convenience init(nodeColor: ZHBBNodeColor, point: CGPoint) {
+        let name = nodeColor.nameFromColor(nodeColor)
+        self.init(imageNamed: name)
+        
+        self.xScale = 4.1
+        self.yScale = 4.1
+        self.color = UIColor.greenColor()
+        self.position = point
+        self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.width / 2.0)
+        self.physicsBody?.dynamic = true
+        self.physicsBody?.affectedByGravity = true
+        self.physicsBody?.contactTestBitMask = nodeColor.contactBitMask(nodeColor)
+        
+        let soundAction = SKAction.playSoundFileNamed("drip.wav", waitForCompletion: false)
+        self.runAction(soundAction)
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let push = SKAction.moveByX(-100, y: 100, duration: 0.3)
+        runAction(push) { () -> Void in
+            
+        }
+    }
+    
+    func remove() {
+//        NSString *sparkPath = [[NSBundle mainBundle] pathForResource:@"PKSpark" ofType:@"sks"];
+//        SKEmitterNode *sparkEmitterNode = [NSKeyedUnarchiver unarchiveObjectWithFile:sparkPath];
+        
+        let path = NSBundle.mainBundle().pathForResource("ZHSpark", ofType: "sks")
+        if let emitter = NSKeyedUnarchiver.unarchiveObjectWithFile(path!) as? SKNode {
+            emitter.position = self.position
+            self.parent?.addChild(emitter)
+            
+            
+
+            let emitterFadeAction = SKAction.fadeAlphaTo(0.0, duration: 0.5)
+            emitter.runAction(emitterFadeAction) { () -> Void in
+                emitter.removeFromParent()
+            }
+        }
+    
+        
+        
+        
+        //let fadeAction = SKAction.fadeAlphaTo(0.0, duration: 0.2)
+        let fadeAction = SKAction.scaleTo(0.2, duration: 0.2)
+        self.runAction(fadeAction) { () -> Void in
+            self.removeFromParent()
+        }
+
+        let soundAction = SKAction.playSoundFileNamed("pew.wav", waitForCompletion: false)
+        self.runAction(soundAction)
+        
+        
+    }
 }
